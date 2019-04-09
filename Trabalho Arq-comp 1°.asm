f95154cadbcb340b52f7a90f96fce18258c40395
JapaBits <34574051+wellingtonj1@users.noreply.github.com>
2019-04-09 10:19:57 -0300

    Update Trabalho Arq-comp 1°.asm

--- Trabalho Arq-comp 1°.asm
+++ Trabalho Arq-comp 1°.asm
@@ -10,13 +10,13 @@ segment .data
 	mens1 db"Digite uma senha de 6 caracteres intercalando maiuscula de minusculas: ",10
 	tam1 equ $-mens1
 	
-	limpatela db 10,10,10,10,10,10
+	limpatela db 27,"[H",27,"[J"
 	limptam equ $-limpatela
 	
 	mens3 db "Tenta advinhar a senha que foi digitada agora!",10
 	tam3 equ $-mens3 
 	
-	mensacerto db" Vocẽ acertou !!!",10
+	mensacerto db"Vocẽ acertou !!!",10
 	tamacerto equ $- mensacerto 
 
 segment .text
@@ -26,7 +26,6 @@ global _start
 _start:
 	
 	;limpa a tela
-	
 	;mov edx,limptam
 	;mov ecx,limpatela
 	;call printstr
@@ -35,22 +34,25 @@ _start:
 	mov ecx,mens1 ;ponteiro da string
 	call printstr
 	
-	mov edx,10 ; maximo armazenado
+	mov edx,7 ; maximo armazenado
 	mov ecx,mens2 ; buffer destino
 	call readstr; Em eax retorna o nº de caracteres armazenados
+	call printstr
 	cmp eax,7 ;Validando a quantidade de caracteres
 	jne _start ;Se for < que 6 volta pro inicio
 	
 	mov ecx,eax ;limite
 	xor esi,esi
 	call valida
-	cmp esi,7
+	cmp esi,6
 	jne _start
 		
+tentativasenha:
+
 	;limpa a tela
-	mov edx,limptam
-	mov ecx,limpatela
-	call printstr
+	;mov edx,limptam
+	;mov ecx,limpatela
+	;call printstr
 	
 	mov edx,tam3 ;quantidade de caracteres, no caso ele imprime o tamanho armazenado em tamm
 	mov ecx,mens3 ;ponteiro da string
@@ -59,23 +61,12 @@ _start:
 	mov edx,100 ; maximo armazenado
 	mov ecx,mens5 ; buffer destino
 	call readstr;Em eax retorna o nº de caracteres armazenados
+	xor esi,esi
 	call comparaduas
 	cmp esi,7
-	jne acerto
-	call finalgeral 
+	jne tentativasenha
+	call acerto
 	
-acerto:
-	
-	mov edx,mensacerto
-	mov ecx,tamacerto
-	call printstr
-	ret
-	
-finalgeral:
-
-	mov eax,1
-	int 80h
-
 segment .bss
 	
 	;dados nao inicializados
@@ -112,15 +103,15 @@ valida:
 	cmp al,"A" ; A C E
 	jb sair
 	cmp al,"Z"
-	jb sair
+	jg sair
 	inc esi 
-	mov ax,[mens2+esi]
+	mov al,[mens2+esi]
 	cmp ax,"a" ; b d f
 	jb sair
 	cmp ax,"z"
-	jb sair
+	jg sair
 	inc esi
-	cmp esi,7
+	cmp esi,6
 	jne valida
 	
 sair:
@@ -129,16 +120,24 @@ sair:
 ;Other Procedure
 comparaduas:
 	
-	xor esi,esi
 	mov al,[mens2+esi]
 	mov ah,[mens5+esi]
 	cmp al,ah
 	jne final
 	inc esi
+	cmp esi,7
+	jne comparaduas
 	
 final:
 
 	ret
 	
+;Other Procedure
+acerto:
+	
+	mov edx,tamacerto
+	mov ecx,mensacerto
+	call printstr
+	ret
 	
 	
