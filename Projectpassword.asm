13912f1a0bc222095de0d5c29d00dc64f7766971
JapaBits <34574051+wellingtonj1@users.noreply.github.com>
2019-05-20 10:50:17 -0300

    Update Projectpassword.asm

--- Projectpassword.asm
+++ Projectpassword.asm
@@ -7,8 +7,10 @@ segment .bss
 	;dados nao inicializados
 	opcao resb 100	
 	mens5 resb 100
+	senha resb 100
 	
 segment .data
+	
 	menu db "**-----------------------------------------------------------------------------",10
 		  db "**		    		Choose a option	 ",10
 		  db "** ( 1 ) access",10,"** ( 2 ) Register ",10,"** ( 3 ) Exit ",10,"** "
@@ -23,17 +25,37 @@ segment .data
 	mens1 db "Digite o nome da pessoa"
 	tammens1 equ $-mens1
 	
-	descritor dd 0
+	erroab db "Erro na abertura do arquivo !!!...Programa fechado",10,10
+	tam equ $-erroab
+	
+	intro1 db "Digite a senha de super usuario para cadastrar uma pessoa ",10
+	tamint equ $-intro1
+	
+	;descritor dd 0 ;variavel de dados
+	fd resd 0
 	
 segment .text
 global _start
 
 _start:
-	call criaarq
+
+	call openarq
+	cmp eax,0	
+	jl errorfile
+	mov [fd],eax
 	
 	mov edx,limptam ;limpa a tela
 	mov ecx,limpatela
-	call printstr
+	call printstr 
+	call impmenu
+
+	
+;Procedure area
+impmenu:
+
+	mov edx,limptam ;limpa a tela
+	mov ecx,limpatela
+	call printstr 
 	
 	mov edx,tamenu ; Imprime menu 
 	mov ecx,menu ;ponteiro da string
@@ -43,27 +65,38 @@ _start:
 	mov ecx,opcao ; disponibiliza o teclado para inserir opção
 	call readstr; Em eax retorna o nº de caracteres armazenados
 	cmp eax,2
-	jne _start
+	jne impmenu
 	
 	call opera
+	ret 
 	
-	call fim
-	
-;Procedure area
 opera:
 
 	mov al,[opcao]
 	cmp al,"1"
 	je acesso
 	cmp al,"2"
-	je cadastro
+	je administra
 	cmp al,"3"
 	je fim
 	
 	cmp al,"0"
-	je _start
+	je impmenu
 	cmp al,"4"
-	jae _start
+	jae impmenu
+	ret
+	
+;Other Procedure
+administra: ;falta validar senha do administrador e inserir nova senha em arquivo
+	mov edx,tamint
+	mov ecx,intro1
+	call printstr
+	
+	mov edx,7
+	mov ecx,senha
+	call readstr
+	cmp eax,7
+	jne impmenu
 	ret
 	
 ;Other Procedure
@@ -81,71 +114,58 @@ readstr:
 	ret
 	
 ;Other Procedure
-criaarq:
+;criaarq:
 	
-	mov ecx,0q777
-	mov ebx,arqname
-	mov eax,8
-	int 80h
-
-	ret
+	;mov ecx,0q777
+	;mov ebx,arqname
+	;mov eax,8
+	;int 80h
+	;ret
 
 ;Other Procedure
 fechaarq:
-	
-	mov ebx,[descritor]
-	mov eax,6
-
+		
+	mov ebx,[fd]
+	mov eax,6 ;serviço closefile
+	int 80h
+	ret
 ;Other Procedure
 openarq:
-	
-	mov edx,0q777
+
+	mov edx,777q
 	mov ecx,2
 	mov ebx,arqname
 	mov eax,5
-	mov [descritor],eax
 	int 80h
-
+	ret
+	
 ;Other Procedure / le o arquivo se a senha inserida for igual a qualquer uma do arquivo
 acesso:
 	
 	ret
 	
 ;Other Procedure /coloca no arquivo
-cadastro:
+;cadastro:
 	
-	call openarq
-	mov edx,tamenu
-	mov ecx,menu
-	mov ebx,[descritor]
-	mov eax,3
-	int 80h
-	
-	call fechaarq
-	ret
+	;call _start
+	;ret
 	
 ;Other Procedure
 fim:
-
+	call fechaarq
 	mov eax,1 ; serviço EXIT
 	int 80h ;encerra (mesmo kernel para executar.. esse é o padrão)
-
 	ret
 
 
 	
 ;Other Procedure em fase de teste
 	 
-	 cmp eax,0
-	 jl errado
-	 mov [fd],eax
+	 ;cmp eax,0
+	 ;jl errado
+	 ;mov [fd],eax
 	
-;encerrar
-
-	mov eax,1
-	int 80h
-	
-;erroab
+errorfile:
 
 	mov edx,tam
 	mov ecx,erroab
@@ -153,12 +173,5 @@ fim:
 	mov eax,4
 	int 80h
 	jmp fim
-	
-;abertura do arquivo
-
-	mov edx,777q
-	mov ecx,2
-	mov ebx,arqname
-	mov eax,5
-	int 80h
+	ret
 	
