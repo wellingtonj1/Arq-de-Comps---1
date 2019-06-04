5576238c68c3f74319f1181c48738a3e35246181
JapaBits <34574051+wellingtonj1@users.noreply.github.com>
2019-06-04 10:43:44 -0300

    Update Trabalho Arq-comp 1°.asm

--- Trabalho Arq-comp 1°.asm
+++ Trabalho Arq-comp 1°.asm
@@ -1,12 +1,19 @@
+
 ;PUC-ECEC-CMP1057-ARQ1-
 ;30/04/19
 ;Wellington Junio De Melo Fernandes 
 ;
 ; filé.asm
+%define BUF_SIZE 256
+
 segment .bss
 	;dados nao inicializados
 	opcao resb 100	
 	senha resb 100
+	poearq resb 100
+	fd_in resd 1
+	fd_out resd 1
+	in_buf resb BUF_SIZE 
 	
 segment .data
 	
@@ -18,8 +25,8 @@ segment .data
 	limpatela db 27,"[H",27,"[J"
 	limptam equ $-limpatela
 	
-	arqname db "arq.sen",0
-	tamarqname equ $-arqname
+	in_file_name db "arq.sen",0
+	tamarqname equ $-in_file_name
 	
 	mens1 db "Digite o nome da pessoa"
 	tammens1 equ $-mens1
@@ -30,8 +37,10 @@ segment .data
 	intro1 db "Digite a senha de super usuario para cadastrar uma pessoa ",10
 	tamint equ $-intro1
 	
+	erro db "Erro aqui nessa posição",10,10
+	tamerro equ $-erro
 	;descritor dd 0 ;variavel de dados
-	fd resd 0
+	
 	
 segment .text
 global _start
@@ -39,10 +48,11 @@ global _start
 _start:
 
 	call openarq
+	mov [fd_in],eax
 	cmp eax,0	
 	jl errorfile
-	mov [fd],eax
 	
+	call poeinicio
 	mov edx,limptam ;limpa a tela
 	mov ecx,limpatela
 	call printstr 
@@ -86,7 +96,7 @@ opera:
 	ret
 	
 ;Other Procedure
-administra: ;falta validar senha do administrador e inserir nova senha em arquivo
+administra: ;falta inserir nova senha em arquivo
 	mov edx,tamint
 	mov ecx,intro1
 	call printstr
@@ -100,47 +110,30 @@ administra: ;falta validar senha do administrador e inserir nova senha em arquiv
 	call valida
 	cmp esi,6
 	jne _start
-	call learq
 	xor esi,esi
-	call compadm
-	
-	ret
-	
-;Other Procedure
-learq:
-	mov edx,1
-	mov ecx,0
-	mov ebx,[fd]
-	mov eax,3
-
-	ret
-
-;Other Procedure
-compadm:
-	
-	mov al,[senha+esi]
-	mov ax,[fd+esi]
-	cmp al,ax
-	jne opera
-	inc esi
+	call compara
 	cmp esi,6
-	jne compadm
+	jne fim
+	mov edx,100
+	mov ecx,poearq
+	call escrevearq
+	
 	ret
 	
 ;Other Procedure
 valida:
-	
+		
 	mov al,[senha+esi] ;car. origem 
 	cmp al,"A" ; A C E
-	jb opera
+	jb impmenu
 	cmp al,"Z"
-	jg opera	
+	jg impmenu	
 	inc esi 
 	mov al,[senha+esi]
 	cmp ax,"a" ; b d f
-	jb opera
+	jb impmenu
 	cmp ax,"z"
-	jg opera
+	jg impmenu
 	inc esi
 	cmp esi,6
 	jne valida
@@ -161,9 +154,18 @@ readstr:
 	ret
 	
 ;Other Procedure
+;criaarq:
+	
+	;mov ecx,0q777
+	;mov ebx,arqname
+	;mov eax,8
+	;int 80h
+	;ret
+
+;Other Procedure
 fechaarq:
 		
-	mov ebx,[fd]
+	mov ebx,[fd_in]
 	mov eax,6 ;serviço closefile
 	int 80h
 	ret
@@ -171,24 +173,18 @@ fechaarq:
 ;Other Procedure
 openarq:
 
-	mov edx,777q
+	mov edx,0700
 	mov ecx,2
-	mov ebx,arqname
+	mov ebx,in_file_name
 	mov eax,5
 	int 80h
 	ret
 	
-;Other Procedure / le o arquivo se a senha inserida for igual a primeira senha do arquivo caso sim,mostra todas as senhas
+;Other Procedure / le o arquivo se a senha inserida for igual a primeira senha do arquivo
 acesso:
 	
 	
 	ret
-	
-;Other Procedure /coloca no arquivo
-;cadastro:
-	
-	;call _start
-	;ret
 	
 ;Other Procedure
 fim:
@@ -203,6 +199,7 @@ fim:
 	 ;jl errado
 	 ;mov [fd],eax
 	
+;Other Procedure
 errorfile:
 
 	mov edx,tam
@@ -211,4 +208,65 @@ errorfile:
 	mov eax,4
 	int 80h
 	jmp fim
+	ret
+
+;Other Procedure
+repeat_read:
+
+	;read input file
+	mov edx,BUF_SIZE
+	mov ecx,in_buf
+	mov ebx,[fd_in]
+	mov eax,3
+	int 80h
+
+	;write to output file
+	mov ecx,in_buf
+	mov ebx,[fd_out]
+	mov eax,4
+	mov edx,eax
+	int 80h
+	cmp edx,BUF_SIZE
+	jl copy_done
+	jmp repeat_read
+	
+	ret
+
+;Other Procedure
+copy_done:
+	
+	mov ebx,[fd_out]
+	mov eax,6
+	ret
+	
+compara:
+	
+	mov al,[senha+esi]
+	mov ah,[fd_in+esi]
+	cmp al,ah
+	jne errototal
+	cmp esi,7
+	jne compara
+	ret
+
+escrevearq:
+
+	mov eax,4
+	mov edx,eax
+	int 80h
+	ret
+
+errototal:
+	
+	mov edx,tamerro
+	mov ecx,erro
+	call printstr
+	ret
+
+poeinicio:
+
+	mov edx,0
+	mov ecx,0
+	mov ebx,[fd_in]
+	mov eax,19
 	ret
